9dc831fd7caa4b07e1bcd379b757d086b2656ce4
Juan Vincent Nugrahaputra <kjuanvincent@gmail.com>
2021-08-17 17:47:10 +0700

    Fixed unicode string issues

--- ProxyDllLoader/dll_proxy.cpp
+++ ProxyDllLoader/dll_proxy.cpp
@@ -12,6 +12,12 @@
     #define __PROXY_MAX_PATH__ 260
 #endif // MAX_PATH
 
+#ifndef UNICODE
+    #define __PROXY_STR__ string
+#else
+    #define __PROXY_STR__ wstring
+#endif // UNICODE
+
 #define DLL_PROXY_ORIGINAL(name) __PROXY_NAMESPACE__::original_##name
 #define DLL_PROXY_ASSERT(condition) if ((condition) == false) return false;
 #define DLL_PROXY_LOAD_RAW(handle, name) DLL_PROXY_ORIGINAL(name) = GetProcAddress(handle, ###name)
@@ -32,8 +38,8 @@ namespace __PROXY_NAMESPACE__
 {
 	static std::filesystem::path get_system_directory()
 	{
-		const auto system_directory(std::make_unique<char[]>(__PROXY_MAX_PATH__));
-		::GetSystemDirectoryA(system_directory.get(), __PROXY_MAX_PATH__);
+		const auto system_directory(std::make_unique<TCHAR[]>(__PROXY_MAX_PATH__));
+		::GetSystemDirectory(system_directory.get(), __PROXY_MAX_PATH__);
 		return std::filesystem::path(system_directory.get());
 	}
 
@@ -42,7 +48,7 @@ namespace __PROXY_NAMESPACE__
 		const auto __PROXY_PATH__ = get_system_directory() / TEXT("xinput9_1_0.dll");
 		DLL_PROXY_ASSERT(std::filesystem::exists(__PROXY_PATH__));
 
-		const auto __PROXY_MODULE__ = ::LoadLibraryW(__PROXY_PATH__.wstring().c_str());
+		const auto __PROXY_MODULE__ = ::LoadLibrary(__PROXY_PATH__.__PROXY_STR__().c_str());
 		DLL_PROXY_ASSERT(__PROXY_MODULE__ != NULL);
 
 		#define __DLL_NAME__ DLL_PROXY_LOAD
